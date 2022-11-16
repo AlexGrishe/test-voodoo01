f82a86cef5d23a9308286ca7656e5156fe139eca
AlexGrishe <sashka.greha@gmail.com>
2022-11-16 23:25:22 +0200

    -> Form-block, next: add map api, method (custom_select), adaptive

---
+++ assets/js/index.js
@@ -0,0 +1,45 @@
+// Написати метод,
+// який знаходитиме на сторінці всі елементи з тегом select,
+// приховуватиме їх,
+// та створюватиме кастомний select згідно з макетом, на місце стандартного select.
+// *не використовувати сторонні бібліотеки
+
+//Делаем метод который находит все теги селект
+//И добавляет к каждому кастомный стиль
+
+
+// //
+// let selectArrPracticeInstitution = [
+//     {name: 'MacDonald\'s'},
+//     {name: 'Tesla'},
+//     {name: 'Silpo'},
+// ]
+//
+// selectArrPracticeInstitution.forEach(e => console.log(e.name))
+// let selectBlocks = document.querySelectorAll('select');
+//
+// for (let selectElement of selectBlocks) {
+//     selectElement.classList.add("test2")
+//     for (let i = 0; i < selectElement.innerHTML.length; i++) {
+//         if (selectElement[i]) {
+//             selectElement[i].classList.add('test3')
+//             console.log(selectElement[i])
+//         }
+//     }
+//     // console.log(selectElement.innerHTML)
+// }
+
+// if (document.querySelector('select.selectArrPracticeInstitution')) {
+//     selectArrPracticeInstitution.forEach(e => bC.innerHTML += "<option>" + e.name + "</option>");
+//     console.log(bC)
+// }
+
+// for (let i = 0; i < bC.length; i++) {
+//     selectArrPracticeInstitution.forEach(e => bC[i].innerHTML += "<option>" + e.name + "</option>");
+//     console.log(bC[i])
+// }
+
+// for (let i = 0; i < elements.length; i++) {
+//     elements[i].innerHTML = `Option from test`
+//     console.log(elements[i].innerHTML)
+// }
